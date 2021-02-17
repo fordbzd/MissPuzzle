14cfad739287e41586660f4e431274c98f6e7c18
Yi <liyi.chn@gmail.com>
2021-02-16 18:21:49 -0800

    Added puzzles.

---
+++ src/main/java/org/misspuzzle/puzzle/leetcode/p100/Q41_FirstMissingPositive.java
@@ -0,0 +1,27 @@
+package org.misspuzzle.puzzle.leetcode.p100;
+
+public class Q41_FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            while (nums[i] != 0 && nums[i] != (i + 1)) {
+                if (nums[i] < 0 || nums[i] > nums.length || nums[nums[i] - 1] == nums[i]) {
+                    nums[i] = 0;
+                } else {
+                    int temp = nums[i];
+                    nums[i] = nums[temp - 1];
+                    nums[temp - 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+}
