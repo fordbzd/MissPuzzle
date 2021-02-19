9d7dc2c78633e5c67e841c4ac3aafe15608d1d0f
Yi <liyi.chn@gmail.com>
2021-02-18 17:52:30 -0800

    Added puzzles.

---
+++ src/test/java/org/misspuzzle/puzzle/leetcode/p500/Q413_Test.java
@@ -0,0 +1,16 @@
+package org.misspuzzle.puzzle.leetcode.p500;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Q413_Test {
+    @Test
+    public void testcase() {
+        final int[] A = {1, 2, 3 ,4};
+
+        final int result = new Q413_NumberOfArithmeticSlices().numberOfArithmeticSlices(A);
+
+        assertEquals(3, result);
+    }
+}
---
+++ src/test/java/org/misspuzzle/puzzle/leetcode/p800/Q795_Test.java
@@ -0,0 +1,16 @@
+package org.misspuzzle.puzzle.leetcode.p800;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Q795_Test {
+    @Test
+    public void testcase() {
+        int[] A = new int[]{2, 1, 4, 3};
+
+        int result = new Q795_NumSubarrayBoundedMax().numSubarrayBoundedMax(A, 2, 3);
+
+        assertEquals(3, result);
+    }
+}
