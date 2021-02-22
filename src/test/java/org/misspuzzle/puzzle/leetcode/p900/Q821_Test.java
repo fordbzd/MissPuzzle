a0060a31e3ee67f5ad4536be5783c5b357071f74
Yi <liyi.chn@gmail.com>
2021-02-21 23:10:48 -0800

    Fixed test.

--- src/test/java/org/misspuzzle/puzzle/leetcode/p900/Q821_Test.java
+++ src/test/java/org/misspuzzle/puzzle/leetcode/p900/Q821_Test.java
@@ -2,13 +2,13 @@ package org.misspuzzle.puzzle.leetcode.p900;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class Q821_Test {
     @Test
     public void testcase() {
         int[] result = new Q821_ShortestToChar().shortestToChar("loveleetcode", 'e');
 
-        assertEquals(new int[]{3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0}, result);
+        assertArrayEquals(new int[]{3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0}, result);
     }
 }
